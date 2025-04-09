7e36f1442beaf2cafff583cb816440f92279326e
Prasadharshe <111993033+Prasadharshe@users.noreply.github.com>
2025-04-09 11:50:15 +0530

    `Update condition for incrementing completed cycles in timer logic`

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@
           setIsTimerRunning(true);
         }
 
-        if ((newCount - startingNumber) %  5 === 0 && newCount !== lastCycleCount) {
+        if ((newCount - startingNumber) %  108 === 0 && newCount !== lastCycleCount) {
           setCompletedCycles((prev) => prev + 1);
           setLastCycleCount(newCount);
           setShowCycleMessage(true);
